56c43dc23f46e387cc854cb3ed293cf5e9a8a140
ChanCanhChua <minh56140@gmail.com>
2025-03-03 16:14:16 +0700

    loginthanhcong

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -26,5 +26,23 @@ namespace WindowsFormsApp1
         {
 
         }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string username = txtUserName.Text;
+            string password = txtPassword.Text;
+
+            if (username == "admin" && password == "admin")
+            {
+                MessageBox.Show("Đăng nhập thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Form2 successForm = new Form2();
+                successForm.Show();
+                this.Hide(); // Ẩn form đăng nhập
+            }
+            else
+            {
+                MessageBox.Show("Sai tên đăng nhập hoặc mật khẩu!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
     }
 }
